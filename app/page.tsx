1badbd68af0ba6053ac15e3c966b57d74c6870c0
Aly Ahmed <gali81375@gmail.com>
2026-06-01 05:43:32 +0200

    improve design

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <h1 style={{padding: 50}}>WORKING 🚀</h1>
+  return (
+    <div style={{ padding: 50, fontFamily: "sans-serif" }}>
+      <h1>AgencyPro 🚀</h1>
+      <p>We build high converting websites for businesses.</p>
+
+      <button style={{ marginTop: 20, padding: 10 }}>
+        Get Started
+      </button>
+    </div>
+  );
 }
